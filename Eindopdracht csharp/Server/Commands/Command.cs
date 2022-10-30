9cef4a59fe0bd8bc41b7f8a3063fdec12cb7fe08
Coen <cc.denboer1@student.avans.nl>
2022-10-30 16:18:55 +0100

    changed command to a struct

--- Eindopdracht csharp/Server/Commands/Command.cs
+++ Eindopdracht csharp/Server/Commands/Command.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Server.Commands
 {
-    public class Command
+    public struct Command
     {
         public string id { get; set; }
         public dynamic data { get; set; }
